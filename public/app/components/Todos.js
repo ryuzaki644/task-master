72a992ef2439806d06a2f0145b4926417611d554
ryuzaki644 <sathwikkoushal@gmail.com>
2017-07-20 08:42:08 +0530

    Add input field to add a todo

--- public/app/components/Todos.js
+++ public/app/components/Todos.js
@@ -1,29 +1,104 @@
 const React = require('react')
-const fetchTodos = require('../actions/fetchTodos')
+const {fetchTodos, postTodos} = require('../actions/TodosAction.js')
 
-class Todos extends React.Component {
+class AddTodo extends React.Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+      todo: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (event) {
+    event.preventDefault()
+    const value = event.target.value
+    this.setState(() => {
+      return {
+        todo: value
+      }
+    })
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    this.props.onSubmit(this.state.todo)
+  }
+
+  render () {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.todo} onChange={this.handleChange} />
+          <button className='button' onSubmit={this.handleSubmit} >
+              Add task Board
+            </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+class ListTodos extends React.Component {
+
+  render () {
+    const todos = this.props.todos
+    return (
+      <div>
+        <ul>
+          {todos.length === 0 ? 'loading' : todos.map(todo => {
+            return <li key={todo.id}>{todo.todo}</li>
+          })
+        }
+        </ul>
+      </div>
+    )
+  }
+}
+
+class Todos extends React.Component {
+
+  constructor (props) {
+    super(props)
+
     this.state = {
       todos: []
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
   componentDidMount () {
-    fetchTodos().then(res => {
-      this.setState(function () {
+    fetchTodos.then(res => {
+      this.setState(() => {
         return {todos: res}
       })
     })
   }
 
+  handleSubmit (todo) {
+    const todoObject = {
+      id: this.state.todos.length + 1,
+      todo: todo,
+      completed: false
+    }
+    this.setState(() => {
+      return {
+        todos: this.state.todos.concat([todoObject])
+      }
+    })
+    postTodos(todoObject).then(todos => todos)
+  }
+
   render () {
-    let todoList = this.state.todos
-    return <div>
-      {this.state.todos.length === 0 ? 'loading' : todoList.map(todo => {
-        return <p
-          key={todo.todo}> {todo.todo} </p>
-      })}
-    </div>
+    return (
+      <div>
+        <AddTodo onSubmit={this.handleSubmit} />
+        <ListTodos todos={this.state.todos} />
+      </div>
+    )
   }
 }
 
