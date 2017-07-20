3f3434eef65cbc418bb2eea4d5a3cfcccbee5811
ryuzaki644 <sathwikkoushal@gmail.com>
2017-07-20 08:50:08 +0530

    Add route board to render the tabular representation of tasks -Use react router Switch to switch between routes

--- public/app/components/App.js
+++ public/app/components/App.js
@@ -2,20 +2,25 @@ const React = require('react')
 const ReactRouter = require('react-router-dom')
 const Router = ReactRouter.BrowserRouter
 const Route = ReactRouter.Route
+const browserHistory = ReactRouter.browserHistory
+const Switch = ReactRouter.Switch
 const Todos = require('./Todos')
+const Taskboard = require('./Taskboard')
 
 class App extends React.Component {
   render () {
     return (
-      <Router >
+      <Router history={browserHistory} >
         <div className='container'>
+          <Switch>
             <Route exact path='/' component={Todos} />
+            <Route exact path='/board' component={Taskboard} />
+          </Switch>
+
         </div>
       </Router>
     )
   }
 }
-
-
 
 module.exports = App
