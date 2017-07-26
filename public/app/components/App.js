12ede4f0d01b918e2215776a5df356c3f3dd0bc7
ryuzaki644 <sathwikkoushal@gmail.com>
2017-07-26 08:53:50 +0530

    Modify App.js and Remove Todos.js

--- public/app/components/App.js
+++ public/app/components/App.js
@@ -2,19 +2,18 @@ const React = require('react')
 const ReactRouter = require('react-router-dom')
 const Router = ReactRouter.BrowserRouter
 const Route = ReactRouter.Route
-const browserHistory = ReactRouter.browserHistory
 const Switch = ReactRouter.Switch
-const Todos = require('./Todos')
+const Taskboards = require('./Taskboards')
 const Taskboard = require('./Taskboard')
 
 class App extends React.Component {
   render () {
     return (
-      <Router history={browserHistory} >
+      <Router >
         <div className='container'>
           <Switch>
-            <Route exact path='/' component={Todos} />
-            <Route exact path='/board' component={Taskboard} />
+            <Route exact path='/' component={Taskboards} />
+            <Route path='/board' component={Taskboard} />
           </Switch>
 
         </div>
